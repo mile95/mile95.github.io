25aeb0ba22f7dc4a39797392c7b22cbcff8bc18b
Fredrik Mile <fredrik.mile@gmail.com>
2025-06-23 07:02:46 +0200

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,23 @@ import { PostService, Post } from './post/posts.service';
 })
 export class AppComponent implements OnInit {
   posts: Post[] = [];
+  tagsToCount = new Map<string, number>();
 
   constructor(private postService: PostService) { }
 
   ngOnInit(): void {
     this.postService.getPosts().subscribe(posts => {
       this.posts = posts;
+      this.calculateTagCounts();
+    });
+  }
+
+  private calculateTagCounts(): void {
+    this.posts.forEach(post => {
+      post.tags.forEach(tag => {
+        const currentCount = this.tagsToCount.get(tag) || 0;
+        this.tagsToCount.set(tag, currentCount + 1);
+      });
     });
   }
 }
