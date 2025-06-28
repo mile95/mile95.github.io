20ccb3729f494714fa649ed0d2052945472619de
Fredrik Mile <fredrik.mile@gmail.com>
2025-06-28 22:39:14 +0200

    remove tmp posts and add first post

--- src/app/post/posts.service.ts
+++ src/app/post/posts.service.ts
@@ -23,7 +23,7 @@ export interface Post {
 @Injectable({ providedIn: 'root' })
 export class PostService {
 
-    private postFilenames = ['example.md', 'example-two.md', 'example-three.md'];
+    private postFilenames = ['announcement.md']
 
     constructor(private http: HttpClient) { }
 
